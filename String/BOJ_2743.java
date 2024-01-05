810ad5e45725fdd6617f3b9af807a33a161ad9f6
ajeong <ajung7038@naver.com>
2024-01-05 11:10:55 +0900

    Feat: BOJ_2743

---
+++ String/BOJ_2743.java
@@ -0,0 +1,15 @@
+package String;
+
+import java.io.*;
+
+public class BOJ_2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        System.out.println(br.readLine().length());
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
