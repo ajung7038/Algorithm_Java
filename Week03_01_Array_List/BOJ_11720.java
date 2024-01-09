9587da99c3c4517a6e89878a7598cfa3e7b83975
ajeong <ajung7038@naver.com>
2024-01-09 23:18:36 +0900

    Feat: BOJ_11720

---
+++ Week03_01_Array_List/BOJ_11720.java
@@ -0,0 +1,22 @@
+package Week03_01_Array_List;
+
+import java.io.*;
+
+public class BOJ_11720 {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine()); // 숫자의 개수
+        String str = br.readLine(); // 공백 없이 주어진 숫자
+
+        int sum = 0; // 전체 합계
+
+        // 숫자 더하기
+        for (int i=0; i<N; i++) {
+            sum += str.charAt(i) -'0';
+        }
+
+        System.out.println(sum);
+        br.close();
+    }
+}
