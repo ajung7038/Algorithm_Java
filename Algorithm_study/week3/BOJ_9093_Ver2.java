918e96e953b564c02256e24518c4860179cf98c1
ajeong <ajung7038@naver.com>
2024-01-17 22:00:47 +0900

    Feat: BOJ_9093_Ver2 - 단어 뒤집기

---
+++ Algorithm_study/week3/BOJ_9093_Ver2.java
@@ -0,0 +1,28 @@
+package Algorithm_study.week3;
+
+import java.io.*;
+import java.util.Stack;
+
+public class BOJ_9093_Ver2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine()); // 테스트 케이스
+
+        while (T-- > 0) {
+            Stack<Character> stack = new Stack<>();
+            String str = br.readLine() + "\n";
+            for (char ch : str.toCharArray()) {
+                if (ch == ' ' || ch == '\n') {
+                    while (!stack.isEmpty()) {
+                        bw.write(stack.pop());
+                    }
+                    bw.write(ch);
+                }
+                else stack.push(ch);
+            }
+        }
+        bw.flush();
+    }
+}
