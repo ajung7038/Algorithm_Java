de7214091ea0476a54cb60a94a5f1f50c16dd9e5
ajeong <ajung7038@naver.com>
2024-01-21 13:47:02 +0900

    Feat: BOJ_1_10807 개수 세기

---
+++ Lv4/BOJ_1_10807.java
@@ -0,0 +1,30 @@
+package Lv4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1_10807 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int V = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        int arr[] = new int[N];
+
+        for (int i=0; i<N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i : arr) {
+            if (i == V) count+=1;
+        }
+
+        System.out.println(count);
+    }
+}
