ec6bf1b62c6713d4eaffb718099a6fc0d258ee58
ajeong <ajung7038@naver.com>
2024-02-20 18:26:59 +0900

    Feat: BOJ_2468_Answer

--- Algorithm_study/week7/BOJ_2468.java
+++ Algorithm_study/week7/BOJ_2468.java
@@ -1,59 +1,79 @@
 package Algorithm_study.week7;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class BOJ_2468 {
+    static List<Integer> list = new ArrayList<>();
+    static int[] dx = {0, 0, -1, 1}; // 상 하 좌 우
+    static int[] dy = {-1, 1, 0, 0}; // 상 하 좌 우
+    static boolean[][] visit;
+    static int[][] arr;
 
-    static int N;
-    static int[][] graph;
-    static boolean[] visited;
-    static int result = 0;
+    static int N, nowX, nowY;
 
-    // 위부터 시계 방향으로 이동
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int max = 0;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
         StringTokenizer st;
-        graph = new int[N][N];
-        visited = new boolean[N*N];
 
-        for (int i=0; i<N; i++) {
+        N = Integer.parseInt(br.readLine());
+
+        // 배열 넣기
+        arr = new int[N][N];
+        for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j=0; j<N; j++) {
-                graph[i][j] = Integer.parseInt(st.nextToken());
-                if (max < graph[i][j]) max = graph[i][j];
-            }
-        }
+            for(int j=0; j<N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
 
-        for (int i=0; i<N; i++) {
-            for (int j=0; j<N; j++) {
-                if (!visited[N*i+j] && graph[i][j] > N) {
-                    DFS(i, j);
-                    result++;
+                // 각 안전 영역의 높이를 저장한다
+                if( !list.contains(arr[i][j])) {
+                    list.add(arr[i][j]);
                 }
             }
         }
 
-        System.out.println(result);
+        // 메모리 줄이기
+        int max = Integer.MIN_VALUE / 16;
+
+        // 안전 영역 계산
+        for(int num : list) {
+            visit = new boolean[N][N];
+            int safeArea = 0;
+
+            for(int i=0; i<N; i++) {
+                for(int j=0; j<N; j++) {
+
+                    // 방문 X && 안전 영역인 경우
+                    if( !visit[i][j] && arr[i][j] >= num) {
+                        DFS(i, j, num);
+                        safeArea++;
+                    }
+                }
+            }
+
+            max = Math.max(max, safeArea);
+        }
+
+        System.out.println(max);
     }
 
-    static void DFS(int x, int y) {
-        visited[N*x+y] = true;
-        for (int i=0; i<4; i++) {
-            int mx = x + dx[i];
-            int my = y + dy[i];
+    static void DFS(int x, int y, int num) {
+        visit[x][y] = true;
 
-            // 범위 안에 있고, 아직 방문하지 않았으며, 잠기지 않은 영역인 경우에만 DFS 호출
-            if (mx >= 0 && mx < N && my >= 0 && my < N) {
-                if (!visited[N*mx+my] && graph[mx][my] > N) DFS(mx, my);
+        for(int i=0; i<4; i++) {
+            nowX = x + dx[i];
+            nowY = y + dy[i];
+
+            // 범위 안 존재 && 방문 X, 안전 영역
+            if(range_check() && !visit[nowX][nowY] && arr[nowX][nowY] >= num) {
+                DFS(nowX, nowY, num);
             }
         }
+
     }
+
+    static boolean range_check() {
+        return (nowX >= 0 && nowY >= 0 && nowX < N && nowY < N);
+    }
+
 }
